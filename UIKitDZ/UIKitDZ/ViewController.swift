cc53b580e4edca4df83fc7f0a865feed5a0ae2af
Киса Мурлыса <kisamurlysa@MacBook-Air-Ola.local>
2024-02-08 19:08:56 +0500

    Убрала отступа

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -5,7 +5,7 @@ import UIKit
 
 /// класс
 class ViewController: UIViewController {
-    lazy var headerImageView = UIImageView(frame: CGRect(x: 0, y: 37, width: 375, height: 818))
+    var headerImageView = UIImageView(frame: CGRect(x: 0, y: 37, width: 375, height: 818))
     var grettingLabel = UILabel(frame: CGRect(x: 0, y: 37, width: 375, height: 82))
     var guessNumberButton = UIButton(frame: CGRect(x: 82, y: 301, width: 150, height: 150))
     var calculatorButton = UIButton(frame: CGRect(x: 132, y: 507, width: 200, height: 200))
@@ -40,22 +40,19 @@ class ViewController: UIViewController {
         view.addSubview(grettingLabel)
         grettingLabel.text = "Приветствую, \n"
         grettingLabel.textAlignment = .center
-        // grettingLabel.font = grettingLabel.font.withSize(30)
         grettingLabel.textColor = .white
-        // grettingLabel.font = grettingLabel.font.fontName
         grettingLabel.backgroundColor = #colorLiteral(red: 0.6520287395, green: 0.83409518, blue: 0.91873914, alpha: 1)
         grettingLabel.font = UIFont(name: "Verdana", size: 30)
         grettingLabel.numberOfLines = 2
-        grettingLabel.font = UIFont.boldSystemFont(ofSize: 30)
-        // grettingLabel.font = UIFont.fontNames(forFamilyName: "Verdana")
+        grettingLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
     }
 
     func configButton(nameButton: UIButton, title: String) {
         view.addSubview(nameButton)
         nameButton.setTitle(title, for: .normal)
         nameButton.layer.cornerRadius = 30
-        nameButton.titleLabel?.font = nameButton.titleLabel?.font.withSize(20)
         nameButton.titleLabel?.textAlignment = .center
+        nameButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
     }
 
     func configColorButton() {
@@ -86,12 +83,17 @@ class ViewController: UIViewController {
         alertController.addTextField { title in
             title.placeholder = "Введите имя"
         }
+
         alertController.addAction(action)
         present(alertController, animated: true)
     }
 
     @objc func showAlertInputNumber() {
-        let alertController = UIAlertController(title: "Введите ваши числа", message: nil, preferredStyle: .alert)
+        let alertController = UIAlertController(
+            title: "Введите ваши числа",
+            message: nil,
+            preferredStyle: .alert
+        )
 
         let actionChange = UIAlertAction(title: "Выбрать операцию", style: .default) { _ in
             let numOne = alertController.textFields?.first
@@ -99,7 +101,7 @@ class ViewController: UIViewController {
             let sum = (Int(numOne?.text ?? "0") ?? 0) + (Int(numTwo?.text ?? "0") ?? 0)
             let sub = (Int(numOne?.text ?? "0") ?? 0) - (Int(numTwo?.text ?? "0") ?? 0)
             let mult = (Int(numOne?.text ?? "0") ?? 0) * (Int(numTwo?.text ?? "0") ?? 0)
-            let div = (Int(numOne?.text ?? "0") ?? 0) / (Int(numTwo?.text ?? "0") ?? 0)
+            let div = (Int(numOne?.text ?? "0") ?? 0) / (Int(numTwo?.text ?? "0") ?? 1)
 
             self.showAlertAction(sum: sum, sub: sub, mult: mult, div: div)
         }
@@ -127,19 +129,19 @@ class ViewController: UIViewController {
         )
 
         let sum = UIAlertAction(title: "Сложить", style: .default) { _ in
-            self.showResultAlert(sum: sum)
+            self.showResultAlert(result: sum)
         }
 
         let subtract = UIAlertAction(title: "Вычесть", style: .default) { _ in
-            self.showResultAlert(sum: sub)
+            self.showResultAlert(result: sub)
         }
 
         let multiply = UIAlertAction(title: "Умножить", style: .default) { _ in
-            self.showResultAlert(sum: mult)
+            self.showResultAlert(result: mult)
         }
 
         let divide = UIAlertAction(title: "Разделить", style: .default) { _ in
-            self.showResultAlert(sum: div)
+            self.showResultAlert(result: div)
         }
 
         let actionCancel = UIAlertAction(title: "Отмена", style: .cancel)
@@ -153,8 +155,8 @@ class ViewController: UIViewController {
         present(alertController, animated: true)
     }
 
-    func showResultAlert(sum: Int) {
-        let alertController = UIAlertController(title: "Ваш результат", message: String(sum), preferredStyle: .alert)
+    func showResultAlert(result: Int) {
+        let alertController = UIAlertController(title: "Ваш результат", message: String(result), preferredStyle: .alert)
         let actionCancel = UIAlertAction(title: "Отмена", style: .default)
         let actionOk = UIAlertAction(title: "Ok", style: .cancel)
 
@@ -164,30 +166,43 @@ class ViewController: UIViewController {
     }
 
     @objc func showAlertGuessNumber() {
-        let alertController = UIAlertController(title: "Угадай число от 1 до 10", message: nil, preferredStyle: .alert)
+        let alertController = UIAlertController(
+            title: "Угадай число от 1 до 10",
+            message: nil,
+            preferredStyle: .alert
+        )
 
         let actionCancel = UIAlertAction(title: "Отмена", style: .default)
-        let actionOk = UIAlertAction(title: "Ok", style: .cancel, handler: { _ in
-            let inputNumber = alertController.textFields?.first
-            let randomNum = Int.random(in: 1 ... 10)
-            if randomNum == Int(inputNumber?.text ?? "0") {
-                self.showAlertTrue(title: "Поздравляю!", message: "Вы угадали число")
-            } else {
-                self.showAlertTrue(title: "Упс!", message: "Это неверный ответ")
+        let actionOk = UIAlertAction(
+            title: "Ok",
+            style: .cancel,
+            handler: { _ in
+                let inputNumber = alertController.textFields?.first
+                let randomNum = Int.random(in: 1 ... 10)
+                if randomNum == Int(inputNumber?.text ?? "0") {
+                    self.showAlertTrue(title: "Поздравляю!", message: "Вы угадали число")
+                } else {
+                    self.showAlertTrue(title: "Упс!", message: "Это неверный ответ")
+                }
             }
-
-        })
+        )
 
         alertController.addTextField { title in
             title.placeholder = "Введите число"
         }
+
         alertController.addAction(actionCancel)
         alertController.addAction(actionOk)
         present(alertController, animated: true)
     }
 
     func showAlertTrue(title: String, message: String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+
         let actionOk = UIAlertAction(title: "Ok", style: .cancel)
 
         alertController.addAction(actionOk)
