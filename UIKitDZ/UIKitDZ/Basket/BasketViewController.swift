e35851ab5d26fd5498a50b293c76cbfe7acc9045
Киса Мурлыса <kisamurlysa@MacBook-Air-Ola.local>
2024-02-16 13:37:55 +0500

    Сделала экран "Корзина", "Профиль"

--- UIKitDZ/UIKitDZ/Basket/BasketViewController.swift
+++ UIKitDZ/UIKitDZ/Basket/BasketViewController.swift
@@ -3,10 +3,320 @@
 
 import UIKit
 
-// Класс корзина
-class BasketViewController: UIViewController {
+/// Экран отображающий корзину с товаром
+final class BasketViewController: UIViewController {
+    // MARK: - Constants
+
+    private enum Constants {
+        static let titleText = "Корзина"
+        static let nameChoes = "Ботинок"
+        static let genderText = "Женские ботинки"
+        static let basket = "Корзина на товаре"
+        static let count = "Количество"
+        static let size = "Размер"
+        static let price = "Цена"
+        static let plus = "+"
+        static let minus = "-"
+        static var countShoes = 1
+        static let sizeThirtyFiveLabel = "35"
+        static let sizeThirtySixLabel = "36"
+        static let sizeThirtySevenLabel = "37"
+        static let sizeThirtyЕightLabel = "38"
+        static let sizeThirtyNineLabel = "39"
+        static var checkoutText = "Оформить заказ - \(sum) ₽"
+        static let priceShoes = 4250
+        static var sum = String(priceShoes)
+        static let basketEmpty = "Ваша корзина пуста"
+        static let transition = "Перейдие в каталог и добавьте товары в корзину"
+        static let bag = "bag"
+    }
+
+    // MARK: - Visual Components
+
+    private let shoesView = UIView()
+    private let shoesImageView = UIImageView()
+
+    private let femaleShoesLabel = UILabel()
+    private let countLabel = UILabel()
+    private let sizeLabel = UILabel()
+    private let priceLabel = UILabel()
+    private let priceShoesLabel = UILabel()
+    private let countSelectLabel = UILabel()
+
+    private let sizeThirtyFiveButton = UIButton()
+    private let sizeThirtySixButton = UIButton()
+    private let sizeThirtySevenButton = UIButton()
+    private let sizeThirtyЕightButton = UIButton()
+    private let sizeThirtyNineButton = UIButton()
+
+    private let plusButton = UIButton()
+    private let minusButton = UIButton()
+    private let checkoutButton = UIButton()
+
+    private let basketButton = UIButton()
+
+    private let basketEmptyLabel = UILabel()
+    private let transitionLabel = UILabel()
+    private let grayView = UIView()
+    private let bagEmtyImageView = UIImageView()
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Корзина"
+        configView()
+        configGroundShoes()
+        configImage()
+        setupLabel()
+        configBasket()
+        setupCountButton()
+        configCountLabel()
+        setupSize()
+        configCheckout()
+        configPriceShoesLabel()
+    }
+
+    // MARK: - Private Methods
+
+    private func configView() {
+        title = Constants.titleText
+    }
+
+    private func configGroundShoes() {
+        view.addSubview(shoesView)
+        shoesView.backgroundColor = #colorLiteral(red: 0.9782040715, green: 0.9782039523, blue: 0.9782040715, alpha: 1)
+        shoesView.translatesAutoresizingMaskIntoConstraints = false
+        shoesView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        shoesView.topAnchor.constraint(equalTo: view.topAnchor, constant: 116).isActive = true
+        shoesView.widthAnchor.constraint(equalToConstant: 157).isActive = true
+        shoesView.heightAnchor.constraint(equalToConstant: 157).isActive = true
+        shoesView.layer.cornerRadius = 12
+    }
+
+    private func configImage() {
+        shoesView.addSubview(shoesImageView)
+        shoesImageView.image = UIImage(named: Constants.nameChoes)
+        shoesImageView.translatesAutoresizingMaskIntoConstraints = false
+        shoesImageView.leftAnchor.constraint(equalTo: shoesView.leftAnchor, constant: 28).isActive = true
+        shoesImageView.topAnchor.constraint(equalTo: shoesView.topAnchor, constant: 29).isActive = true
+        shoesImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        shoesImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+
+    private func configLabel(nameLabel: UILabel, text: String, top: CGFloat) {
+        view.addSubview(nameLabel)
+        nameLabel.text = text
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 193).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: top).isActive = true
+        nameLabel.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 15).isActive = true
+        nameLabel.font = UIFont.systemFont(ofSize: 12)
+    }
+
+    private func setupLabel() {
+        configLabel(nameLabel: femaleShoesLabel, text: Constants.genderText, top: 134)
+        configLabel(nameLabel: countLabel, text: Constants.count, top: 161)
+        configLabel(nameLabel: sizeLabel, text: Constants.size, top: 188)
+        configLabel(nameLabel: priceLabel, text: Constants.price, top: 258)
+    }
+
+    private func configBasket() {
+        shoesView.addSubview(basketButton)
+        basketButton.setImage(UIImage(named: Constants.basket), for: .normal)
+        basketButton.translatesAutoresizingMaskIntoConstraints = false
+        basketButton.leftAnchor.constraint(equalTo: shoesView.leftAnchor, constant: 127).isActive = true
+        basketButton.topAnchor.constraint(equalTo: shoesView.topAnchor, constant: 11).isActive = true
+        basketButton.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        basketButton.heightAnchor.constraint(equalToConstant: 19).isActive = true
+        basketButton.addTarget(self, action: #selector(clearViews), for: .touchUpInside)
+    }
+
+    private func setupSize() {
+        configSize(nameButton: sizeThirtyFiveButton, text: Constants.sizeThirtyFiveLabel, left: 199)
+        configSize(nameButton: sizeThirtySixButton, text: Constants.sizeThirtySixLabel, left: 234)
+        configSize(nameButton: sizeThirtySevenButton, text: Constants.sizeThirtySevenLabel, left: 269)
+        configSize(nameButton: sizeThirtyЕightButton, text: Constants.sizeThirtyЕightLabel, left: 305)
+        view.addSubview(sizeThirtyNineButton)
+        sizeThirtyNineButton.setTitle(Constants.sizeThirtyNineLabel, for: .normal)
+        sizeThirtyNineButton.translatesAutoresizingMaskIntoConstraints = false
+        sizeThirtyNineButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 199).isActive = true
+        sizeThirtyNineButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 234).isActive = true
+        sizeThirtyNineButton.widthAnchor.constraint(equalToConstant: 31).isActive = true
+        sizeThirtyNineButton.heightAnchor.constraint(equalToConstant: 17).isActive = true
+        sizeThirtyNineButton.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+        sizeThirtyNineButton.backgroundColor = #colorLiteral(red: 0.9856336713, green: 0.9391976595, blue: 0.9656866193, alpha: 1)
+        sizeThirtyNineButton.clipsToBounds = true
+        sizeThirtyNineButton.setTitleColor(.black, for: .normal)
+        sizeThirtyNineButton.layer.cornerRadius = 10
+        sizeThirtyNineButton.titleLabel?.textAlignment = .center
+    }
+
+    private func configCheckout() {
+        view.addSubview(checkoutButton)
+        checkoutButton.setTitle(Constants.checkoutText, for: .normal)
+        checkoutButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        checkoutButton.backgroundColor = #colorLiteral(red: 0.9155070186, green: 0.2173944712, blue: 0.5847681761, alpha: 1)
+        checkoutButton.layer.cornerRadius = 12
+        checkoutButton.translatesAutoresizingMaskIntoConstraints = false
+        checkoutButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        checkoutButton.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        checkoutButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        checkoutButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 654).isActive = true
+    }
+
+    private func configBasketView() {
+        view.addSubview(grayView)
+        grayView.backgroundColor = #colorLiteral(red: 0.9782040715, green: 0.9782039523, blue: 0.9782040715, alpha: 1)
+        grayView.translatesAutoresizingMaskIntoConstraints = false
+        grayView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 152).isActive = true
+        grayView.topAnchor.constraint(equalTo: view.topAnchor, constant: 290).isActive = true
+        grayView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        grayView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        grayView.layer.cornerRadius = 12
+        grayView.layer.borderWidth = 0.7
+        grayView.layer.borderColor = UIColor.black.cgColor
+    }
+
+    private func configBag() {
+        grayView.addSubview(bagEmtyImageView)
+        bagEmtyImageView.image = UIImage(named: Constants.bag)
+        bagEmtyImageView.translatesAutoresizingMaskIntoConstraints = false
+        bagEmtyImageView.leftAnchor.constraint(equalTo: grayView.leftAnchor, constant: 19).isActive = true
+        bagEmtyImageView.topAnchor.constraint(equalTo: grayView.topAnchor, constant: 17).isActive = true
+        bagEmtyImageView.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        bagEmtyImageView.heightAnchor.constraint(equalToConstant: 36).isActive = true
+    }
+
+    private func configLabelEmpty() {
+        view.addSubview(basketEmptyLabel)
+        basketEmptyLabel.text = Constants.basketEmpty
+        basketEmptyLabel.translatesAutoresizingMaskIntoConstraints = false
+        basketEmptyLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 80).isActive = true
+        basketEmptyLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 378).isActive = true
+        basketEmptyLabel.widthAnchor.constraint(equalToConstant: 215).isActive = true
+        basketEmptyLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        basketEmptyLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        basketEmptyLabel.textAlignment = .center
+    }
+
+    private func configTransitionLabel() {
+        view.addSubview(transitionLabel)
+        transitionLabel.text = Constants.transition
+        transitionLabel.translatesAutoresizingMaskIntoConstraints = false
+        transitionLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50).isActive = true
+        transitionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 422).isActive = true
+        transitionLabel.widthAnchor.constraint(equalToConstant: 275).isActive = true
+        transitionLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        transitionLabel.font = UIFont.systemFont(ofSize: 16)
+        transitionLabel.textAlignment = .center
+        transitionLabel.numberOfLines = 2
+        transitionLabel.textColor = #colorLiteral(red: 0.4905452132, green: 0.5193168521, blue: 0.5706592202, alpha: 1)
+    }
+
+    private func configCountButton(nameButton: UIButton, text: String, left: CGFloat) {
+        view.addSubview(nameButton)
+        nameButton.setTitle(text, for: .normal)
+        nameButton.translatesAutoresizingMaskIntoConstraints = false
+        nameButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: left).isActive = true
+        nameButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 162).isActive = true
+        nameButton.widthAnchor.constraint(equalToConstant: 15).isActive = true
+        nameButton.heightAnchor.constraint(equalToConstant: 15).isActive = true
+        nameButton.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+        nameButton.layer.cornerRadius = 10
+        nameButton.backgroundColor = #colorLiteral(red: 0.9856336713, green: 0.9391976595, blue: 0.9656866193, alpha: 1)
+        nameButton.setTitleColor(.black, for: .normal)
+    }
+
+    private func setupCountButton() {
+        configCountButton(nameButton: plusButton, text: Constants.plus, left: 333)
+        configCountButton(nameButton: minusButton, text: Constants.minus, left: 295)
+        plusButton.addTarget(self, action: #selector(increaseCount), for: .touchUpInside)
+        minusButton.addTarget(self, action: #selector(decreaseCount), for: .touchUpInside)
+    }
+
+    private func configCountLabel() {
+        view.addSubview(countSelectLabel)
+        countSelectLabel.text = String(Constants.countShoes)
+        countSelectLabel.translatesAutoresizingMaskIntoConstraints = false
+        countSelectLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 313).isActive = true
+        countSelectLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 162).isActive = true
+        countSelectLabel.widthAnchor.constraint(equalToConstant: 17).isActive = true
+        countSelectLabel.heightAnchor.constraint(equalToConstant: 15).isActive = true
+        countSelectLabel.textAlignment = .center
+    }
+
+    func configPriceShoesLabel() {
+        view.addSubview(priceShoesLabel)
+        priceShoesLabel.text = String(Constants.priceShoes) + " P"
+        priceShoesLabel.translatesAutoresizingMaskIntoConstraints = false
+        priceShoesLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 296).isActive = true
+        priceShoesLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 250).isActive = true
+        priceShoesLabel.widthAnchor.constraint(equalToConstant: 115).isActive = true
+        priceShoesLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        priceShoesLabel.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+    }
+
+    private func configSize(nameButton: UIButton, text: String, left: CGFloat) {
+        view.addSubview(nameButton)
+        nameButton.setTitle(text, for: .normal)
+        nameButton.translatesAutoresizingMaskIntoConstraints = false
+        nameButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: left).isActive = true
+        nameButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 213).isActive = true
+        nameButton.widthAnchor.constraint(equalToConstant: 31).isActive = true
+        nameButton.heightAnchor.constraint(equalToConstant: 17).isActive = true
+        nameButton.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+        nameButton.backgroundColor = #colorLiteral(red: 0.9856336713, green: 0.9391976595, blue: 0.9656866193, alpha: 1)
+        nameButton.clipsToBounds = true
+        nameButton.layer.cornerRadius = 10
+        nameButton.titleLabel?.textAlignment = .center
+        nameButton.setTitleColor(.black, for: .normal)
+        nameButton.addTarget(self, action: #selector(changeBorder), for: .touchUpInside)
+    }
+
+    @objc private func changeBorder(_ sender: UIButton) {
+        sender.layer.borderWidth = 0.7
+        sender.layer.borderColor = #colorLiteral(red: 0.9155070186, green: 0.2173944712, blue: 0.5847681761, alpha: 1)
+    }
+
+    @objc private func clearViews() {
+        shoesView.removeFromSuperview()
+        shoesImageView.removeFromSuperview()
+        femaleShoesLabel.removeFromSuperview()
+        countLabel.removeFromSuperview()
+        sizeLabel.removeFromSuperview()
+        priceLabel.removeFromSuperview()
+        countSelectLabel.removeFromSuperview()
+        sizeThirtyFiveButton.removeFromSuperview()
+        sizeThirtySixButton.removeFromSuperview()
+        sizeThirtySevenButton.removeFromSuperview()
+        sizeThirtyЕightButton.removeFromSuperview()
+        sizeThirtyNineButton.removeFromSuperview()
+        plusButton.removeFromSuperview()
+        minusButton.removeFromSuperview()
+        checkoutButton.removeFromSuperview()
+        basketButton.removeFromSuperview()
+        priceShoesLabel.removeFromSuperview()
+
+        configLabelEmpty()
+        configBasketView()
+        configBag()
+        configTransitionLabel()
+    }
+
+    @objc private func increaseCount() {
+        Constants.countShoes += 1
+        Constants.sum = String(Constants.priceShoes * Constants.countShoes)
+        Constants.checkoutText = "Оформить заказ - \(Constants.sum) ₽"
+        configCountLabel()
+        checkoutButton.setTitle(Constants.checkoutText, for: .normal)
+    }
+
+    @objc func decreaseCount() {
+        Constants.countShoes -= 1
+        Constants.sum = String((Int(Constants.sum) ?? 0) - Constants.priceShoes)
+        Constants.checkoutText = "Оформить заказ - \(Constants.sum) ₽"
+        configCountLabel()
+        checkoutButton.setTitle(Constants.checkoutText, for: .normal)
     }
 }
--- UIKitDZ/UIKitDZ/Profile/UserDataViewController.swift
+++ UIKitDZ/UIKitDZ/Profile/UserDataViewController.swift
@@ -3,5 +3,201 @@
 
 import UIKit
 
-// Класс данные пользователя
-class UserDataViewController: UIViewController {}
+/// Экран отображающий данные пользователя
+final class UserDataViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let titleText = "Мои данные"
+        static let backText = "Стрелка"
+
+        static let name = "Имя"
+        static let surname = "Фамилия"
+        static let size = "Размер ноги"
+        static let birth = "Дата рождения"
+        static let email = "Почта"
+        static let phone = "Номер телефона"
+        static let save = "Сохранить"
+
+        static let maxNumCount = 11
+    }
+
+     // MARK: - Visual Components
+    
+    private  let backButton = UIButton()
+    private  let saveButton = UIButton()
+    
+    private  let nameTextField = UITextField()
+    private  let surnameTextField = UITextField()
+    private  let phoneTextField = UITextField()
+    private  let sizeTextField = UITextField()
+    private  let birthTextField = UITextField()
+    private  let emailTextField = UITextField()
+    
+    private  let datePicker = UIDatePicker()
+    private  let titleLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configBack()
+        setupTextField()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configBirth()
+        configView()
+    }
+
+    // MARK: - Private Methods
+    
+    private func configView() {
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        titleLabel.text = Constants.titleText
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 130).isActive = true
+        titleLabel.widthAnchor.constraint(equalToConstant: 115).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 53).isActive = true
+        titleLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+    }
+
+    private func configBack() {
+        view.addSubview(backButton)
+        backButton.setImage(UIImage(named: Constants.backText), for: .normal)
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 22).isActive = true
+        backButton.widthAnchor.constraint(equalToConstant: 9).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: 15).isActive = true
+        backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 63).isActive = true
+        backButton.addTarget(self, action: #selector(showProfile), for: .touchUpInside)
+    }
+
+    private func configTextField(nameTextField: UITextField, text: String, top: CGFloat) {
+        view.addSubview(nameTextField)
+        nameTextField.placeholder = text
+
+        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        nameTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        nameTextField.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        nameTextField.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        nameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: top).isActive = true
+        nameTextField.layer.cornerRadius = 12
+        nameTextField.backgroundColor = #colorLiteral(red: 0.9782040715, green: 0.9782039523, blue: 0.9782040715, alpha: 1)
+    }
+
+    private func setupTextField() {
+        configTextField(nameTextField: nameTextField, text: Constants.name, top: 113)
+        nameTextField.delegate = self
+        configTextField(nameTextField: surnameTextField, text: Constants.surname, top: 167)
+        configTextField(nameTextField: phoneTextField, text: Constants.phone, top: 221)
+        phoneTextField.textContentType = .telephoneNumber
+        phoneTextField.delegate = self
+        configTextField(nameTextField: sizeTextField, text: Constants.size, top: 275)
+        configTextField(nameTextField: birthTextField, text: Constants.birth, top: 329)
+        configTextField(nameTextField: emailTextField, text: Constants.email, top: 383)
+        emailTextField.textContentType = .emailAddress
+        emailTextField.delegate = self
+        phoneTextField.accessibilityIdentifier = "phoneTextField"
+        emailTextField.accessibilityIdentifier = "emailTextField"
+        nameTextField.accessibilityIdentifier = "nameTextField"
+    }
+
+    private func formatPhoneNumber(with mask: String, phone: String) -> String {
+        let numbers = phone.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+        var result = ""
+        var index = numbers.startIndex // numbers iterator
+
+        for ch in mask where index < numbers.endIndex {
+            if ch == "X" {
+                result.append(numbers[index])
+
+                index = numbers.index(after: index)
+
+            } else {
+                result.append(ch)
+            }
+        }
+        return result
+    }
+
+    private func formatMailAdress(with mailDomain: String, adress: String) -> String {
+        guard !adress.contains("@") else { return adress }
+        return adress + "@\(mailDomain)"
+    }
+
+    private func configSave() {
+        view.addSubview(saveButton)
+        saveButton.setTitle(Constants.save, for: .normal)
+        saveButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        saveButton.backgroundColor = #colorLiteral(red: 0.9155070186, green: 0.2173944712, blue: 0.5847681761, alpha: 1)
+        saveButton.layer.cornerRadius = 12
+        saveButton.addTarget(self, action: #selector(closeButton), for: .touchUpInside)
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        saveButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        saveButton.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        saveButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        saveButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 654).isActive = true
+    }
+
+    private func configBirth() {
+        birthTextField.inputView = datePicker
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .automatic
+        birthTextField.delegate = self
+
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        let rightBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        toolBar.setItems([rightBarButton, doneButton], animated: true)
+        birthTextField.inputAccessoryView = toolBar
+    }
+
+    @objc private func doneAction() {
+        let formater = DateFormatter()
+        formater.dateFormat = "dd.MM.yyyy"
+        birthTextField.text = formater.string(from: datePicker.date)
+        view.endEditing(true)
+        birthTextField.resignFirstResponder()
+    }
+
+    @objc private func closeButton() {
+        saveButton.removeFromSuperview()
+    }
+
+    @objc private func showProfile() {
+        dismiss(animated: true)
+    }
+}
+
+///Расширение для контроля введеного текста в полях ввода: Имя, Номер телефона, Эл. почта
+extension UserDataViewController: UITextFieldDelegate {
+
+    /// - TextField: принимаемое поля ввода
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        //проверяем наличие текста в поле ввода
+        guard
+            let textFieldText = textField.text,
+            !textFieldText.isEmpty
+        else { return }
+
+        //в зависимости от идентификатора текстфилда выплняем определенную операция после введенного текста
+        switch textField.accessibilityIdentifier {
+        case "phoneTextField":
+            textField.text = formatPhoneNumber(with: "+X (XXX) XXX-XXXX", phone: textFieldText)
+        case "emailTextField":
+            textField.text = formatMailAdress(with: "gmail.com", adress: textFieldText)
+        case "nameTextField":
+            configSave()
+        default: break
+        }
+    }
+}
